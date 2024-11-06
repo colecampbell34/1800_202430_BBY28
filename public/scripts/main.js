bfa789eca4583d2db5650b74993304ffae794500
Clinton Nguyen <nguyen.clinton@outlook.com>
2024-11-06 06:14:42 -0800

    added placeholder for progress bar on home page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -19,8 +19,6 @@ function getNameFromAuth() {
 getNameFromAuth(); //run the function
 
 // Retrieve groups collection a user is in and find nearest trip date
+// TODO
 const userId = firebaseConfigbase.auth().currentUser.uid;
 const groups = db.collection("users").doc(userId).collection("groups");
-function findNearestDeadline(groups) {
-
-}
