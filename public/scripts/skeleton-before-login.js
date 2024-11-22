0a1110b976668b9df4c4ec6c2579de10cb5790b5
Cole Campbell <ccampbell211@my.bcit.ca>
2024-11-22 11:58:46 -0800

    removed link to main.html from landing page, changed button to say "log in now"

---
+++ public/scripts/skeleton-before-login.js
@@ -0,0 +1,6 @@
+//Loads the header and navbar
+function loadSkeleton() {
+    ($("#header").load('skeleton/header-before-login.html'));
+    ($("#navbar").load('skeleton/navbar.html'));
+}
+loadSkeleton();
